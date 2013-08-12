4b84a192904065e54f168980444812440ef82073
toran billups <toranb@gmail.com>
2013-08-11 21:22:29 -0500

    added undo

--- js/app.js
+++ js/app.js
@@ -3,6 +3,6 @@ App = Ember.Application.create();
 App.ApplicationController = Ember.Controller.extend({
     liked: false,
     cliq: function() {
-        this.set('liked', true);
+        this.set('liked', !this.liked);
     }
 });
